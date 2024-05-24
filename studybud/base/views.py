1b1ed5455ca1fdf76de0f30ea7df46fed68e3b3e
Dario Gjoka <dario.gjoka@sciant.com>
2024-05-24 10:06:45 +0200

    added changes

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,22 @@
+from typing import Optional
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render 
 
+rooms: list[dict] = [
+    {'id': 1, 'name': 'Learn Python'},
+    {'id': 2, 'name': 'Learn C#'},
+    {'id': 3, 'name': 'Learn Java'}
+]
+
 def home(request: HttpRequest) -> HttpResponse:
-    ...
+    return render(request, 'base/home.html', {'rooms': rooms})
+    
+
+def room(request: HttpRequest, pk: str) -> HttpResponse:
+    room: Optional[dict] = None
+    for r in rooms:
+        if r['id'] == int(pk):
+            room = r
+            break
+    context = { 'room': room }
+    return render(request, 'base/room.html', context)
