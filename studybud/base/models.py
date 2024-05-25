3ec6948edf3b6ff013f8cec3c589b4e55b96c177
Dario Gjoka <dario.gjoka@sciant.com>
2024-05-25 20:52:54 +0200

    Added changes to models

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,11 +1,16 @@
+from ast import For, mod
 from django.db import models
 
 class User(models.Model):
     first_name = models.CharField('first_name', max_length=20)
     last_name = models.CharField('last_name', max_length=20)
-    email = models.CharField('email', max_length=256, null=False)
-    password = models.CharField('password', max_length=256, null=False)
+    email = models.CharField('email', max_length=256, null=False, unique=True, blank=False)
+    password = models.CharField('password', max_length=256, null=False, blank=False)
     age = models.IntegerField('age', default=0)
+    blog_count = models.IntegerField('blog_count', default=0)
+
+    class Meta:
+        db_table = 'User'
 
     def __str__(self) -> str:
         return f"<User first_name={self.first_name} last_name={self.last_name} age={self.age}>\n"
@@ -16,11 +21,15 @@ class User(models.Model):
     
 
 class Blog(models.Model):
+    
     blogger = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     blog_text = models.TextField('blog_text')
     publication_date = models.DateTimeField('publication_date')
     like_count = models.IntegerField('like_count', default=0)
     comment_count = models.IntegerField('comment_count', default=0)
+
+    class Meta:
+        db_table = 'Blog'
 
     def __str__(self) -> str:
         return f"<Blog blogger={self.blogger.full_name} blog_text={self.blog_text}"
@@ -32,5 +41,19 @@ class Comment(models.Model):
     like_count = models.IntegerField('like_count', default=0)
     comment_text = models.TextField('comment_text')
 
+    class Meta:
+        db_table = 'Comment'
+
     def __str__(self) -> str:
         return f"<Comment commenter={self.commenter.full_name}"    
+
+class Like(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    liker = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Like'
+        
+    def __str__(self) -> str:
+        return f"<Like comment={self.comment} liker={self.liker.full_name}>"
+    
